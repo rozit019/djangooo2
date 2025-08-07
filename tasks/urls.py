22e80d8f4dcc1a829f9b601edf2646f5347dff8c
rozit019 <rozitaryal70@gmail.com>
2025-08-07 07:59:13 +0545

    first_commit

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,5 +1,7 @@
 from django .urls import path
 from . import views
 urlpatterns = [
-    path('', views.index, name='index'),#views.index indx chai functioj name
+    path('', views.index, name='index'),
+    path('api/posts/', views.get_posts,name='get_posts'),
+        #   views.index indx chai functioj name
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -2,7 +2,20 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from .models import BlogPost
 from django.core.serializers import serialize
+import json
 
 # Create your views here.
 def index(request):
     return render(request, 'tasks/post_list.html')
+
+def get_posts(request):
+    #
+    posts = BlogPost.objects.all().order_by('-published')
+    #saves the data from the admin site in following format
+    data = [{
+        'title':post.title,
+        'content': post.content,
+        'published': post.published.strftime('%Y-%m-%d %H:%M:%S'),#date time save grna
+    }for post in posts] #hamle backend bata fetch grera layeko data lai list ma rakhyo
+    
+    return JsonResponse(data, safe=False)  # safe=False allows non-dict objects to be serialized
